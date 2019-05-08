b9b527a1c22587d0931209597c1cad749cc2b306
Taite Sandefer <taite.sandefer@gmail.com>
2019-05-08 10:13:22 -0600

    imports

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import re
 import pickle
+import pymongo
 
 from sklearn.model_selection import train_test_split
 from gensim.models.doc2vec import TaggedDocument
